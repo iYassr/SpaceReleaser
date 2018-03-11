ca7572c241b54c99ffd79faca1c14ba6d955859a
root <root@YasserPC.localdomain>
2018-03-11 16:46:50 +0300

    refactoring

--- TopSizeFiles.py
+++ TopSizeFiles.py
@@ -5,13 +5,13 @@ import os.path
 import operator
 import argparse
 
-parser = argparse.ArgumentParser(description="printing the largest files in your system")
-parser.add_argument('-p', '-path', type=str, default='C:\\')
-parser.add_argument('-d', '-delete', type=int)
-parser.add_argument('-t', '-top', type=int, default=20)
-parser.add_argument('-f', '-full', action='store_true', default=False)
-args = parser.parse_args()
-
+def parse_args():
+    parser = argparse.ArgumentParser(description="printing the largest files in your system")
+    parser.add_argument('-p', '--path', type=str, default='C:\\', help='provide path to search into', metavar='path')
+    parser.add_argument('-d', '--delete', type=int, help='index of the file to be deleted', metavar='file index')
+    parser.add_argument('-t', '--top', type=int, default=20, help='how many files to display', metavar='number of files')
+    parser.add_argument('-f', '--full', action='store_true', default=False, help='to display the full path of the file')
+    return parser.parse_args()
 
 def to_mb(file_size):
     return '%d MB' % (file_size/100)
@@ -35,35 +35,40 @@ def longest_string(list, top, full):
     return maxstr
 
 
-fileSizes = {}
-try:
-    for path, subFolders, files in os.walk(args.p):
-        for fileName in files:
+def main():
+    args = parse_args()
 
-            fileSize = os.path.getsize(os.path.join(path, fileName))
-            FileName = os.path.join(path, fileName)
-            fileSizes[FileName] = fileSize
-except Exception:
-    pass
-sorted_fileSizes = sorted(fileSizes.items(), key=operator.itemgetter(1))
-sorted_fileSizes.reverse()
-justing = longest_string(sorted_fileSizes, args.t, args.f)
-for i in range(args.t):
-    if args.f:
-        fileName = sorted_fileSizes[i][0]
-    else:
-        fileName = sorted_fileSizes[i][0].split('\\')[-1]
-    fileSize = to_mb(sorted_fileSizes[i][1])
-    counter = '[%s]' % i
-    print(counter.ljust(4), fileName.ljust(justing, '-'), fileSize)
+    fileSizes = {}
+    try:
+        for path, subFolders, files in os.walk(args.path):
+            for fileName in files:
+
+                fileSize = os.path.getsize(os.path.join(path, fileName))
+                FileName = os.path.join(path, fileName)
+                fileSizes[FileName] = fileSize
+    except Exception:
+        pass
+    sorted_files = sorted(fileSizes.items(), key=operator.itemgetter(1))
+    sorted_files.reverse()
+    justing = longest_string(sorted_files, args.top, args.full)
+    for i in range(args.top):
+        if args.full:
+            fileName = sorted_files[i][0]
+        else:
+            fileName = sorted_files[i][0].split('\\')[-1]
+        fileSize = to_mb(sorted_files[i][1])
+        counter = '[%s]' % i
+        print(counter.ljust(4), fileName.ljust(justing, '-'), fileSize)
 
 
-if args.d is not None:
-    print('do you want do delete %s? yes or no' % sorted_fileSizes[args.d][0])
-    yesno = input()
-    if str(yesno) == 'yes':
-        delete_file(sorted_fileSizes[args.d][0])
-        print(sorted_fileSizes[args.d][0] + ' Has been deleted')
-    else:
-        print('You chose no, exiting...')
-        exit()
+    if args.delete is not None:
+        print('do you want do delete %s? yes or no' % sorted_files[args.delete][0])
+        yesno = input()
+        if str(yesno) == 'yes':
+            delete_file(sorted_files[args.delete][0])
+            print(sorted_files[args.delete][0] + ' Has been deleted')
+        else:
+            print('You chose no, exiting...')
+            exit()
+
+main()
